f7fe402287540fde78e3ba99afeb0df856fccc0e
alozta <alozta@gmail.com>
2016-09-08 16:36:26 +0300

    Dummy request handler

---
+++ src/main/java/com/imarine/WebService/WebService.java
@@ -0,0 +1,45 @@
+package com.imarine.WebService;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by alozta on 9/8/16.
+ */
+@EnableAutoConfiguration
+@RestController
+public class WebService {
+
+    //ApplicationContext ctx = new AnnotationConfigApplicationContext(MongoConfig.class);
+    //MongoOperations mongoOperation = (MongoOperations)ctx.getBean("mongoTemplate");     //interface that allows mongodb operations
+    private MongoOperations mongoOperation;
+
+    WebService(){
+        mongoOperation = MongoConfig.getMongoTemplate();
+    }
+
+    public MongoOperations mongo() {
+        return mongoOperation;
+    }
+
+
+    @RestController
+    public class AlarmController {
+
+        private static final String template = "Hello, %s!";
+        private final AtomicLong counter = new AtomicLong();
+
+        @RequestMapping("/saved_alarms")        //Use @RequestMapping(method=GET) to narrow this mapping.
+        public SavedAlarms saved_alarms(@RequestParam(value="type", defaultValue="none") String type,
+                                        @RequestParam(value="operation", defaultValue="none") String operation,
+                                        @RequestParam(value="data1", defaultValue="none") String data1,
+                                        @RequestParam(value="data2", defaultValue="none") String data2) {
+            return new SavedAlarms(counter.toString(),type,operation,data1,data2);
+        }
+    }
+}
