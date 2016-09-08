e96b80e8502190be1a0a49a1ea3ad06d2d47847d
alozta <alozta@gmail.com>
2016-09-08 16:37:59 +0300

    Spring compatible mongoDB configuration

--- src/main/java/com/imarine/WebService/MongoConfig.java
+++ src/main/java/com/imarine/WebService/MongoConfig.java
@@ -1,26 +1,50 @@
 package com.imarine.WebService;
 
 import com.mongodb.MongoClient;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
 import java.net.UnknownHostException;
 
 /**
  * Created by alozta on 9/7/16.
  */
+@EnableAutoConfiguration
+@Configuration
 public class MongoConfig {
 
     private static MongoConfig mongoConfig;
     private static MongoClient mongoClient;
+    private static MongoTemplate mongoTemplate;
+    private static String db_name = "alarm_db";
+
+    public static @Bean MongoDbFactory mongoDbFactory() throws Exception {
+        return new SimpleMongoDbFactory(mongoClient, db_name);
+    }
+
+    public static @Bean MongoTemplate mongoTemplate() throws Exception {
+
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
+
+        return mongoTemplate;
+
+    }
 
     public static void changeMongoClient(String ip, int port){
-        try {
-            mongoClient=new MongoClient(ip,port);
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
+        mongoClient=new MongoClient(ip,port);
     }
 
     public static MongoClient getMongoClient() {
         return mongoClient;
+    }
+
+    public static MongoTemplate getMongoTemplate() {
+        getInstance();
+        return mongoTemplate;
     }
 
     /**
@@ -29,6 +53,11 @@ public class MongoConfig {
     public static MongoConfig getInstance(){
         if(mongoConfig==null){
             mongoConfig = new MongoConfig();
+            try {
+                mongoTemplate=mongoTemplate();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return mongoConfig;
     }
@@ -37,10 +66,6 @@ public class MongoConfig {
      * Default: Local mongoDB configuration
      * */
     private MongoConfig(){
-        try {
-            mongoClient=new MongoClient("localhost",27017);
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
+        mongoClient=new MongoClient("localhost",27017);
     }
 }
--- src/main/java/com/imarine/WebService/Test.java
+++ src/main/java/com/imarine/WebService/Test.java
@@ -1,11 +1,42 @@
 package com.imarine.WebService;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by alozta on 9/7/16.
  */
+@SpringBootApplication
+@EnableAutoConfiguration
 public class Test {
 
     public static void main(String [] args){
+        WebService ws = new WebService();
 
+        //ws.mongo().save(new SavedAlarms("smuggling","create","data","data"));
+
+        // query to search user
+        //Query searchUserQuery = new Query(Criteria.where("username").is("mkyong"));
+
+        // query to search user
+        //Query searchUserQuery = new Query(Criteria.where("username").is("mkyong"));
+
+        // find the saved user again.
+        //User savedUser = mongoOperation.findOne(searchUserQuery, User.class);
+
+        // update password
+        //mongoOperation.updateFirst(searchUserQuery, Update.update("password", "new password"),User.class);
+
+        // find the updated user object
+        //User updatedUser = mongoOperation.findOne(searchUserQuery, User.class);
+
+        // delete
+        //mongoOperation.remove(searchUserQuery, User.class);
+
+        // List, it should be empty now.
+        //List<User> listUser = mongoOperation.findAll(User.class);
+
+        SpringApplication.run(WebService.class, args);
     }
 }
